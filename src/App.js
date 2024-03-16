f9f5af0f22cc4c82e010c0773797cb8714f1925c
waqas2714 <waqasali00123@gmail.com>
2024-03-17 00:05:49 +0500

    react-router-dom configured and wto pages linked

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Chat from './pages/Chat.js';
+import Signup from './pages/Signup.js';
+import Login from './pages/Login.js';
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
