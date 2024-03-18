0e791211761063319bf98fb3a3b152f9d24acfda
waqas2714 <waqasali00123@gmail.com>
2024-03-19 01:17:40 +0500

    new page added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import {
 import Chat from './pages/Chat.js';
 import Signup from './pages/Signup.js';
 import Login from './pages/Login.js';
+import Home from './pages/Home.js';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path='/chat' element={<Chat />} />
+        {/* <Route path='/chat' element={<Chat />} /> */}
         <Route path='/signup' element={<Signup />} />
         <Route path='/' element={<Login />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </Router>
   )
