191e1ce686ca693dc10d1113b1792e43cc34a965
waqas2714 <waqasali00123@gmail.com>
2024-03-19 01:18:00 +0500

    toastify added with options

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+export const toastOptions = {
+  position: "top-right",
+  autoClose: 3500,
+  hideProgressBar: true, 
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  theme: "dark"
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <ToastContainer />
   </React.StrictMode>
 );
