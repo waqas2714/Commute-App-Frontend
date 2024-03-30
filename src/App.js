781f8d95de80469b2814d577c67552c586d76487
waqas2714 <waqasali00123@gmail.com>
2024-03-30 22:34:37 +0500

    Added Driver's Signup Page

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Chat from './pages/Chat.js';
 import Signup from './pages/Signup.js';
 import Login from './pages/Login.js';
 import Home from './pages/Home.js';
+import SignupDriver from './pages/SignupDriver.js';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Route path='/signup' element={<Signup />} />
         <Route path='/' element={<Login />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/signupDriver' element={<SignupDriver />} />
       </Routes>
     </Router>
   )
