b28cb7880f2616b3fdb445e757bc8875d521e5e6
waqas2714 <waqasali00123@gmail.com>
2024-04-12 11:29:56 +0500

    chat integration done. Iw's working now!

--- src/components/ChatBoxTag.js
+++ src/components/ChatBoxTag.js
@@ -1,27 +1,71 @@
+import axios from "axios";
 import React from "react";
+import { backendUrl } from "../utils/backendUrl";
+import { toastOptions } from "..";
+import { toast } from "react-toastify";
 
-const ChatBoxTag = () => {
-    const isDriver = true;
+const ChatBoxTag = ({from, to, date, time, listingId, isDriver, setOpenChat, setChat, setDestination, setDeparture, setChatListing, socket, scrollToBottom}) => {
+ 
+  const dateFormat = (dateString) => {
+    const date = new Date(dateString);
+
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+
+    const formattedDate = `${day}-${month}-${year}`;
+
+    return formattedDate;
+  };
+
+  date = dateFormat(date);
+
+  const truncateString = (str) => {
+    return str.length > 10 ? str.slice(0, 10) + "..." : str;
+  };
+
+  const getChat = async ()=>{
+    try {
+      const {data} = await axios.get(`${backendUrl}/api/chat/getChat/${listingId}`);
+
+      if (!data.success) {
+        return toast.error("Could not find chat for this listing, please try again.", toastOptions);
+      }
+
+      socket.emit("join-room", listingId);
+      
+      console.log(data.chats);
+      setChat(data.chats);
+      setDeparture(from);
+      setDestination(to);
+      const timeoutId = setTimeout(scrollToBottom, 100);
+      setOpenChat(true); 
+      setChatListing(listingId);     
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className={`flex flex-wrap justify-between p-3 gap-y-3 sm:w-[46vw] md:w-[33vw] md:max-w-[290px] mt-2 ${isDriver ? "bg-[#94cebb] hover:bg-[#4CE5B1] text-black" : "bg-[#161616] hover:bg-[#2c2c2c] text-white"}  cursor-pointer  transition-all ease-in-out duration-150`}>
-      <div className="flex align-baseline gap-1 w-[45%]">
+    <div className={` flex flex-wrap justify-between p-3 gap-y-3 sm:w-[46vw] md:w-[33vw] md:max-w-[290px] mt-2 ${isDriver ? "bg-[#94cebb] hover:bg-[#4CE5B1] text-black" : "bg-[#161616] hover:bg-[#2c2c2c] text-white"}  cursor-pointer  transition-all ease-in-out duration-150`} onClick={getChat}>
+      <div className="flex align-baseline gap-1 w-[45%] text-sm">
         <h2>From: </h2>
-        <p>DHA</p>
+        <p>{truncateString(from)}</p>
       </div>
 
-      <div className="flex align-baseline gap-1 w-[45%]">
+      <div className="flex align-baseline gap-1 w-[45%] text-sm">
         <h2>To: </h2>
-        <p>Isb</p>
+        <p>{truncateString(to)}</p>
       </div>
 
-      <div className="flex align-baseline gap-1 w-[45%]">
+      <div className="flex align-baseline gap-1 w-[45%] text-sm">
         <h2>Date: </h2>
-        <p>01-07-24</p>
+        <p>{date}</p>
       </div>
 
-      <div className="flex align-baseline gap-1 w-[45%]">
+      <div className="flex align-baseline gap-1 w-[45%] text-sm">
         <h2>Time: </h2>
-        <p>03:00</p>
+        <p>{time}</p>
       </div>
     </div>
   );
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -5,25 +5,69 @@ import Navbar from "../components/Navbar";
 import ChatBoxTag from "../components/ChatBoxTag";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { MdSend } from "react-icons/md";
+import axios from "axios";
+import { toastOptions } from "..";
+import { toast } from "react-toastify";
 
 const socket = io(backendUrl);
 const Chat = () => {
   const [chat, setChat] = useState([]);
+  const [chatTags, setChatTags] = useState([]);
+  const [chatTagsLoading, setChatTagsLoading] = useState(true);
+  const [openChat, setOpenChat] = useState(false);
   const [message, setMessage] = useState("");
+  const [destination, setDestination] = useState("");
+  const [departure, setDeparture] = useState("");
+  const [chatListing, setChatListing] = useState("");
+  const [scrollToBot, setScrollToBot] = useState(false);
   const isListeningRef = useRef(false);
-
-  const handleSubmit = (e) => {
+  const userId = JSON.parse(localStorage.getItem("user"))._id;
+  const username = JSON.parse(localStorage.getItem("user")).username;
+  const chatRef = useRef(null)
+  const handleSubmit = async (e) => {
+    e.stopPropagation();
     e.preventDefault();
-    socket.emit("send-msg", { msg: message });
-    setChat((prevState) => [...prevState, message]);
-    setMessage("");
-    console.log(chat);
+    if (message !== "") {
+      try {
+        const { data } = await axios.post(
+          `${backendUrl}/api/chat/addChat/${chatListing}/${userId}`,
+          { message }
+        );
+        if (!data.success) {
+          return toast.error(data.error, toastOptions);
+        }
+        socket.emit("send-msg", { message, username, _id: userId, listingId : chatListing });
+        setChat((prevState) => [...prevState, { message, username, _id: userId }]);
+        setMessage("");
+        setScrollToBot(!scrollToBot);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  const getChatTags = async () => {
+    try {
+      const { data } = await axios.get(
+        `${backendUrl}/api/chat/getAllChats/${userId}`
+      );
+      if (!data.success) {
+        return toast.error(
+          "Chats could not be found. Please try again.",
+          toastOptions
+        );
+      }
+      setChatTags(data.chats);
+      setChatTagsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(() => {
     if (!isListeningRef.current) {
       socket.on("msg-recieve", (msg) => {
-        console.log("message recieved : " + msg);
+        console.log(msg);
         setChat((prevState) => {
           return [...prevState, msg];
         });
@@ -32,110 +76,159 @@ const Chat = () => {
     }
   });
 
+  useEffect(() => {
+    getChatTags();
+  }, []);
+
+  // Scroll to the bottom of the chat window
+  const scrollToBottom = () => {
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  };
+
+  useEffect(() => {
+
+  // Delay scrolling to the bottom to ensure message is rendered
+  const timeoutId = setTimeout(scrollToBottom, 100);
+
+  // Clean up
+  return () => clearTimeout(timeoutId);
+  }, [scrollToBot]);
+
   return (
     <>
       <Navbar />
       <div className="mt-[10vh] h-[90vh] p-[2vw] bg-black text-white overflow-hidden">
-        <h1 className="text-center text-4xl font-bold text-white md:w-[300px]">Chat</h1>
+        <h1 className="text-center text-4xl font-bold text-white md:w-[300px]">
+          Chat
+        </h1>
 
         <div className="relative flex">
-          <div className="mt-2 md:mt-0 max-h-[75vh]  w-[96vw] overflow-auto sm:flex sm:flex-wrap sm:gap-x-[1vw] md:block md:w-[35vw] md:max-w-[305px]">
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
-            <ChatBoxTag />
+          <div className="mt-2 md:mt-0 max-h-[75vh]  w-[96vw] overflow-auto sm:flex sm:flex-wrap sm:gap-x-[1vw] md:block md:w-[34vw] md:max-w-[305px]">
+            {chatTagsLoading ? (
+              <p>Loading...</p>
+            ) : chatTags.length > 0 ? (
+              chatTags.map((tag, index) => (
+                <ChatBoxTag
+                  from={tag.departure}
+                  to={tag.destination}
+                  date={tag.date}
+                  time={tag.time}
+                  listingId={tag.listingId}
+                  isDriver={tag.isDriver}
+                  setOpenChat={setOpenChat}
+                  setChat={setChat}
+                  setDestination={setDestination}
+                  setDeparture={setDeparture}
+                  setChatListing={setChatListing}
+                  socket={socket}
+                  scrollToBottom={scrollToBottom}
+                  key={index}
+                />
+              ))
+            ) : (
+              <p className="text-center text-[#4CE5B1] text-xl font-semibold">
+                No Chats Available.
+              </p>
+            )}
           </div>
 
-          <div className="w-[100vw] h-[100vh]  absolute translate-x-[-2vw] 
-              translate-y-[-6vh]
-              md:translate-x-[300px] 
-              md:h-[81vh]
-              md:w-[65vw]
-              lg:w-[72vw]
-              "
+          <div
+            className={`
+            ${openChat ? "translate-x-[-2vw]" : "translate-x-[102vw]"}
+            translate-y-[2vh]
+            w-[100vw] 
+            h-[90vh]  
+            absolute   
+            md:h-[74vh]
+            chatbox
+            chatbox-translate
+            transition-all
+            ease-in-out
+            duration-150
+              `}
           >
+            {
+              chatListing.length > 0 ? /* Heading  */
+              <>
+              <div className=" h-[15%] flex justify-between items-center md:px-6 bg-[#111111]">
+                <div className="w-[12%] h-full bg-[#303030] hover:bg-[#585858] transition-all ease-in-out duration-200 flex justify-center items-center cursor-pointer md:hidden  ">
+                  <IoMdArrowRoundBack
+                    size={35}
+                    onClick={() => setOpenChat(false)}
+                  />
+                </div>
+  
+                <div className="w-[87%] flex flex-col  ">
+                  <div className="flex gap-x-1 items-baseline text-lg">
+                    <h1>From:</h1>
+                    <p className="sidebar-content w-[80%]">{departure}</p>
+                  </div>
+                  <div className="flex gap-x-1 items-baseline text-lg">
+                    <h1>To:</h1>
+                    <p className="sidebar-content w-[80%]">{destination}</p>
+                  </div>
+                </div>
+              </div>
+              
+              {/* Chat Part  */}
+              <div className="bg-[#161616] h-[75%]">
+                <div ref={chatRef} className="flex flex-col h-[85%] md:h-[95%] p-4 overflow-auto scrollbar">
+                  
+                  {chat.length > 0 ? (
+                    chat.map((chat, index) => {
+                      return (
+                        <div
+                          className={`${
+                            userId == chat._id && "self-end"
+                          } w-fit max-w-[60%] md:max-w-[50%]`}
+                          key={index}
+                        >
+                          <h5
+                            className={`text-[#4CE5B1] ${
+                              userId == chat._id && "text-right"
+                            }`}
+                          >
+                            {userId == chat._id ? "Me" : chat.username}
+                          </h5>
+                          <p className=" bg-[#303030] p-3 rounded-lg">
+                            {chat.message}
+                          </p>
+                        </div>
+                      );
+                    })
+                  ) : (
+                    <h2 className="text-center text-[#4CE5B1] text-xl font-semibold">
+                      Send the first message.
+                    </h2>
+                  )}
+                </div>
+  
+                {/* Input  */}
+                <div className=" h-[15%] md:h-[19%]">
+                  <form
+                    className="bg-[#111111] relative h-[100%] flex justify-center items-center"
+                    onSubmit={handleSubmit}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Message..."
+                      className="text-black text-lg p-3 pr-[52px] w-[90%] rounded-full border-0 outline-none"
+                      value={message}
+                      onChange={(e)=>setMessage(e.target.value)}
+                    />
+                    <MdSend
+                      size={55}
+                      className="absolute right-3 sm:right-7 md:right-6 lg:right-9 bg-black p-2 rounded-full cursor-pointer hover:text-[#4CE5B1]"
+                      type="submit"
+                      onClick={handleSubmit}
+                    />
+                  </form>
+                </div>
+              </div> </>: <p className="text-center text-[#4CE5B1] text-xl font-semibold">Select an inbox to chat</p>
+            }
             
-              
-            {/* Heading  */}
-            <div className="bg-[#161616] h-[15%] flex justify-between items-center">
-              <div className="w-[12%] h-full bg-[#303030] flex justify-center items-center cursor-pointer ">
-                <IoMdArrowRoundBack size={35} />
-              </div>
-              {/* <div className="flex gap-x-1 items-baseline w-[39%]  text-xl">
-                <h1>From:</h1>
-                <p className="sidebar-content">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-              </div>
-              <div className="flex gap-x-1 items-baseline w-[39%]  text-xl">
-                <h1>To:</h1>
-                <p className="sidebar-content">NUST</p>
-              </div> */}
-              <div className="w-[87%] flex flex-col  ">
-              <div className="flex gap-x-1 items-baseline text-lg">
-                <h1>From:</h1>
-                <p className="sidebar-content">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-              </div>
-              <div className="flex gap-x-1 items-baseline text-lg">
-                <h1>To:</h1>
-                <p className="sidebar-content">NUST</p>
-              </div>
-              </div>
-            </div>
-            {/* Chat Part  */}
-            <div className="bg-[#161616] h-[75%]">
-                
-              <div className="flex flex-col h-[83%] md:h-[99%] p-4 overflow-auto scrollbar">
-
-                <div>
-                    <h5 className="text-[#4CE5B1]">Maaz</h5>
-                    <p className="max-w-[60%] bg-[#303030] p-3 rounded-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quia optio obcaecati ame</p>
-                </div>
-
-                <div className="self-end">
-                    <h5 className="text-[#4CE5B1] text-right">Me</h5>
-                    <p className="max-w-[60vw] bg-[#303030] p-3 rounded-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quia optio obcaecati ame</p>
-                </div>
-
-                <div>
-                    <h5 className="text-[#4CE5B1]">Maaz</h5>
-                    <p className="max-w-[60vw] bg-[#303030] p-3 rounded-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quia optio obcaecati ame</p>
-                </div>
-
-                <div className="self-end">
-                    <h5 className="text-[#4CE5B1] text-right">Me</h5>
-                    <p className="max-w-[60vw] bg-[#303030] p-3 rounded-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quia optio obcaecati ame</p>
-                </div>
-
-                <div>
-                    <h5 className="text-[#4CE5B1]">Maaz</h5>
-                    <p className="max-w-[60vw] bg-[#303030] p-3 rounded-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore quia optio obcaecati ame</p>
-                </div>
-
-
-              </div>
-              
-              {/* Input  */}
-              <div className=" h-[15%]">
-                <form className="bg-[#111111] relative h-[100%] flex justify-center items-center">
-                    <input type="text" placeholder="Message..." className="text-black text-lg p-3 pr-[52px] w-[90%] rounded-full border-0 outline-none"/>
-                    <MdSend size={55} className="absolute right-3 sm:right-7 md:right-6 lg:right-9 bg-black p-2 rounded-full cursor-pointer hover:text-[#4CE5B1]" />
-                </form>
-            </div>
-          </div>
           </div>
         </div>
       </div>
